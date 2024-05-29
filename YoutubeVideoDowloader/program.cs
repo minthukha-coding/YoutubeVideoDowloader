3f42e0ec85f5935b37e371d9d74623881c00bf82
minthukha <minthukha1239788@gmail.com>
2024-05-30 00:34:12 +0630

    Add project files.

---
+++ YoutubeVideoDowloader/program.cs
@@ -0,0 +1,58 @@
+﻿using YoutubeExplode;
+
+namespace YouTubeDownloader
+{
+    public class Program
+    {
+        static async Task Main()
+        {
+            string outputDirectory = @"D:\YouTubeVideo";
+
+            List<string> videoUrls = new List<string>
+            {
+
+                "https://www.youtube.com/watch?v=w-viEajTqXU",
+            };
+            try
+            {
+                foreach (var videoUrl in videoUrls)
+                {
+                    await DownloadYouTubeVideo(videoUrl, outputDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while downloading the videos: " + ex.Message);
+            }
+        }
+        static async Task DownloadYouTubeVideo(string videoUrl, string outputDirectory)
+        {
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(videoUrl);
+
+            string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
+
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
+            var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
+
+            if (muxedStreams.Any())
+            {
+                var streamInfo = muxedStreams.First();
+                using var httpClient = new HttpClient();
+                var stream = await httpClient.GetStreamAsync(streamInfo.Url);
+                var datetime = DateTime.Now;
+
+                string outputFilePath = Path.Combine(outputDirectory, $"{sanitizedTitle}.{streamInfo.Container}");
+                using var outputStream = File.Create(outputFilePath);
+                await stream.CopyToAsync(outputStream);
+
+                Console.WriteLine("Download completed!");
+                Console.WriteLine($"Video saved as: {outputFilePath}{datetime}");
+            }
+            else
+            {
+                Console.WriteLine($"No suitable video stream found for {video.Title}.");
+            }
+        }
+    }
+}
